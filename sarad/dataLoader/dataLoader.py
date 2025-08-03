cb62e4ef40038fcb5031515b3039fb994f23e7fe
talexm <talexm@infibond.com>
2025-08-03 08:28:40 +0300

    update

---
+++ sarad/dataLoader/dataLoader.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+import numpy as np
+
+class DataLoader:
+
+    def __init__(self):
+        pass
+
+    def load_data_s2(base_dir = '/kaggle/input/sentinel12-image-pairs-segregated-by-terrain/v_2/'):
+        image_data = []
+
+        for root, dirs, files in os.walk(base_dir):
+            # Only include files if their immediate directory is named 's2'
+            if os.path.basename(root).lower() == "s2":
+                for file in files:
+                    if file.lower().endswith('.png'):
+                        image_path = os.path.join(root, file)
+                        try:
+                            with Image.open(image_path) as img:
+                                img_array = np.array(img)
+                            label = os.path.relpath(root, base_dir)
+                            image_data.append({'image': img_array, 'label': label})
+                        except Exception as e:
+                            print(f"Error loading image {image_path}: {e}")
+
+        return image_data
