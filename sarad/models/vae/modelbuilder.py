1f8efd32ac0563c06f7ece85eefbebd99611be90
talexm <talexm@infibond.com>
2025-08-04 12:34:06 +0300

    update

--- sarad/models/vae/modelbuilder.py
+++ sarad/models/vae/modelbuilder.py
@@ -79,7 +79,7 @@ class ModelBuilder:
 
         model = Model(inputs, outputs)
         model.compile(optimizer=Adam(1e-4), loss='binary_crossentropy')
-        return model, mean
+        return model
 
 
     def train_model(self, model, X_train, X_val):
