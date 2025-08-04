e56169a797d39eab6ae5a2faf04e807290ba1a76
talexm <talexm@infibond.com>
2025-08-04 16:16:11 +0300

    update

--- sarad/models/vae/modelbuilder.py
+++ sarad/models/vae/modelbuilder.py
@@ -39,27 +39,25 @@ class ModelBuilder:
         pass
 
     def build_model(self, X_train):
+        ### build model
         input_shape = X_train.shape[1:]
         inputs = Input(shape=input_shape)
-
         x = inputs
-        filters = 64
-        n_layers = 5  # 7 is too aggressive unless you're resizing images
 
         # ENCODER
+        filters = 64
+        n_layers = 7
+        latent_dim = 64
+
         for _ in range(n_layers):
-            x = Conv2D(filters, (3, 3), padding='same')(x)
-            x = BatchNormalization()(x)
-            x = LeakyReLU()(x)
+            x = Conv2D(filters, (3, 3), activation='relu', padding='same')(x)
             x = MaxPooling2D((2, 2), padding='same')(x)
-            filters *= 2
+            filters *= 2  # grow deeper layers
 
         shape_before_flattening = K.int_shape(x)[1:]
         x_flat = Flatten()(x)
-        x_flat = Dropout(0.2)(x_flat)
 
         # LATENT SPACE
-        latent_dim = 128  # bump up for multiband detail
         mean = Dense(latent_dim, name="z_mean")(x_flat)
         log_var = Dense(latent_dim, name="z_log_var")(x_flat)
         z = Sampling()([mean, log_var])
@@ -70,15 +68,13 @@ class ModelBuilder:
 
         filters //= 2
         for _ in range(n_layers):
-            x = Conv2DTranspose(filters, (3, 3), strides=2, padding='same')(x)
-            x = BatchNormalization()(x)
-            x = LeakyReLU()(x)
+            x = Conv2DTranspose(filters, (3, 3), strides=2, activation='relu', padding='same')(x)
             filters //= 2
 
         outputs = Conv2D(input_shape[-1], (3, 3), activation='sigmoid', padding='same')(x)
 
         model = Model(inputs, outputs)
-        model.compile(optimizer=Adam(1e-4), loss='mse')
+        model.compile(optimizer=Adam(1e-4), loss='binary_crossentropy')
         return model
 
 
